6fe3f150dab69afbd8e46410cdda2907f91f375b
Tomas-Silva-PT <81096021+Tomas-Silva-PT@users.noreply.github.com>
2025-04-13 01:36:10 +0100

    feat(website): add wizard for livedemo pt. 2

--- website/src/components/LiveDemoForm/index.js
+++ website/src/components/LiveDemoForm/index.js
@@ -2,8 +2,9 @@ import clsx from "clsx";
 import Heading from "@theme/Heading";
 import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
-import React, { useEffect, useState } from 'react';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import React, { useEffect, useState } from "react";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import { ArrowRight, Upload, X } from "lucide-react";
 
 const steps = [
   {
@@ -28,33 +29,31 @@ const steps = [
 const brokers = [
   {
     name: "Trading212",
-    logo: "/img/brokers/trading212.png"
+    logo: "/img/brokers/trading212.png",
   },
   {
     name: "eToro",
-    logo: "/img/brokers/etoro.png"
+    logo: "/img/brokers/etoro.png",
   },
   {
     name: "XTB",
-    logo: "/img/brokers/xtb.png"
+    logo: "/img/brokers/xtb.png",
   },
   {
     name: "Degiro",
-    logo: "/img/brokers/degiro.png"
+    logo: "/img/brokers/degiro.png",
   },
   {
     name: "Revolut",
-    logo: "/img/brokers/revolut.png"
-  }
+    logo: "/img/brokers/revolut.png",
+  },
 ];
-
-
-
 
 export default function LiveDemoForm() {
   const [step, setStep] = useState(1);
   const [broker, setBroker] = useState({});
   const [progress, setProgress] = useState(0);
+  const [files, setFiles] = useState([]);
 
   useEffect(() => {
     let newProgress = ((step - 1) / 2) * 100;
@@ -66,13 +65,13 @@ export default function LiveDemoForm() {
     return (
       <>
         <div className={clsx(styles.header)}>
-        <HeaderSteps step={step}/>
-        <HeaderProgress step={step}/>
+          <HeaderSteps step={step} />
+          <HeaderProgress step={step} />
         </div>
       </>
     );
   }
-  
+
   function HeaderSteps(props) {
     let currStep = props.step;
     return (
@@ -90,7 +89,7 @@ export default function LiveDemoForm() {
       </>
     );
   }
-  
+
   function HeaderStep(props) {
     let currStep = props.step;
     let num = props.num;
@@ -98,99 +97,145 @@ export default function LiveDemoForm() {
     let description = props.description;
     return (
       <>
-        <div key={num} className={clsx(styles.headerStep, currStep >= num ? styles.headerStepActive : styles.headerStepInactive)}>
+        <div
+          key={num}
+          className={clsx(
+            styles.headerStep,
+            currStep >= num
+              ? styles.headerStepActive
+              : styles.headerStepInactive
+          )}
+        >
           <div className={clsx(styles.headerStepNum)}>{num}</div>
           <div className={clsx(styles.headerStepTitle)}>{title}</div>
         </div>
       </>
     );
   }
-  
+
   function HeaderProgress(props) {
     let currStep = props.step;
     return (
       <>
-        <div
-          className={clsx(styles.headerProgressContainer)}
-        >
-          <div className={clsx(styles.headerProgress)}
-               style={{ width: `${progress}%` }}>
-          </div>
+        <div className={clsx(styles.headerProgressContainer)}>
+          <div
+            className={clsx(styles.headerProgress)}
+            style={{ width: `${progress}%` }}
+          ></div>
         </div>
       </>
     );
   }
-  
+
   function Content(props) {
     let currStep = props.step;
     let currStepData = steps[currStep - 1];
     let description = currStepData.description;
     return (
       <>
-          <div className={clsx(styles.content)}>
-            <h3>{description}:</h3>
-            {
-              currStepData.num === 1 && 
-              (
-                <ContentStep1 />
-              )
-            }
-            {
-              currStepData.num === 1 && 
-              (
-                <ContentStep2 />
-              )
-            }
-            {
-              currStepData.num === 1 && 
-              (
-                <ContentStep3 />
-              )
-            }
-          </div>
+        <div className={clsx(styles.content)}>
+          <h3>{description}:</h3>
+          {currStepData.num === 1 && <ContentStep1 />}
+          {currStepData.num === 2 && <ContentStep2 />}
+          {currStepData.num === 3 && <ContentStep3 />}
+        </div>
       </>
-    )
+    );
   }
-  
+
   function onBrokerSelected(broker) {
     setStep((step) => step + 1);
     setBroker(broker);
   }
-  
+
   function ContentStep1(props) {
-    
     return (
       <>
         <div className={clsx(styles.contentStep1)}>
           {brokers.map((broker) => (
-            <div className={clsx(styles.contentStep1Broker)} onClick={ () => onBrokerSelected(broker)}>
-              <img style={{ width: "100%", height: "auto" }} alt={broker.name} src={useBaseUrl(broker.logo)} />
+            <div
+              className={clsx(styles.contentStep1Broker)}
+              onClick={() => onBrokerSelected(broker)}
+            >
+              <img
+                style={{ width: "100%", height: "auto" }}
+                alt={broker.name}
+                src={useBaseUrl(broker.logo)}
+              />
             </div>
           ))}
         </div>
       </>
     );
   }
-  
+
+  function onFileUpload(e) {
+    const files = e.target.files;
+    if (files) {
+      setFiles((prev) => [...prev, ...Array.from(files)]);
+    }
+  }
+
+  function removeFile(index) {
+    setFiles((prev) => prev.filter((_, i) => i !== index));
+  }
+
+  function onFilesSelected () {
+    if (files.length === 0 || !broker) return;
+    setStep((step) => step + 1);
+  }
+
   function ContentStep2(props) {
-  
     return (
       <>
+        <div className={clsx(styles.contentStep2)}>
+          <Upload className={clsx(styles.contentStep2UploadIcon)} />
+          <input
+            class={clsx(styles.contentStep2UploadInput)}
+            id="file-upload"
+            type="file"
+            accept=".csv"
+            onChange={onFileUpload}
+            multiple
+          />
+          <label htmlFor="file-upload">Escolher ficheiro(s)</label>
+          {files.length > 0 && (
+            <>
+              <div className={clsx(styles.contentStep2Files)}>
+                {files.map((file, index) => (
+                  <div key={index} className={clsx(styles.contentStep2File)}>
+                    <div className={clsx(styles.contentStep2FileName)}>
+                      {file.name}
+                    </div>
+                    <div
+                      className={clsx(styles.contentStep2FileRemove)}
+                      onClick={() => removeFile(index)}
+                    >
+                      <X/>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className={clsx(styles.contentStep2Process)} onClick={onFilesSelected}>
+              <div className={clsx(styles.contentStep2ProcessText)}>
+                Processar {files.length} ficheiro{files.length !== 1 ? "s" : ""}
+              </div>
+              <ArrowRight/>
+              </div>
+            </>
+          )}
+        </div>
       </>
     );
-   }
-  
-  function ContentStep3(props) { 
-  
-    return (
-      <>
-      </>
-    );
+  }
+
+  function ContentStep3(props) {
+    return <></>;
   }
   return (
     <>
       <section className={clsx(styles.form)}>
-        <Header step={step}/>
+        <Header step={step} />
         <Content step={step} />
       </section>
     </>
