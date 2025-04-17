8704658025d183a3eff5ed209d1dc4fa4eec4353
Tomas-Silva-PT <81096021+Tomas-Silva-PT@users.noreply.github.com>
2025-04-17 23:14:21 +0100

    fix(website): add delayed api fetching
    There is a problem when you try to use a CORS public proxy, and that is the rate limit. For that reason a limit is imposed on API calls. The CORS proxy is only applied when the system recognizes a CORS block was applied to the request. A preferred solution would be a server dedicated to communicating with APIs, but the purpose of this application is to be able to work completely on the client side.

--- website/src/maisvalias-tool/models/yahoofinance.js
+++ website/src/maisvalias-tool/models/yahoofinance.js
@@ -1,4 +1,6 @@
 class YahooFinance {
+  static corsProblem = false;
+
   static async getExchangeRate(fromCurrency, toCurrency, date) {
     const ticker = `${fromCurrency}${toCurrency}=X`;
     let exchangeDate = new Date(date);
@@ -7,34 +9,80 @@ class YahooFinance {
 
     let unixDate = Math.floor(exchangeDate.getTime() / 1000);
     let unixNextDate = Math.floor(nextExchangeDate.getTime() / 1000);
+    let data;
+    let url;
+    try {
+      if(this.corsProblem) { throw new Error("CORS problem"); }
+      url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?period1=${unixDate}&period2=${unixNextDate}&interval=1d`;
+      let response = await fetch(url);
+      data = await response.json();
+    } catch (error) {
+      this.corsProblem = true;
+      url =
+        "https://api.allorigins.win/get?url=" +
+        encodeURIComponent(
+          "https://query1.finance.yahoo.com/v8/finance/chart/" +
+            ticker +
+            "?period1=" +
+            unixDate +
+            "&period2=" +
+            unixNextDate +
+            "&interval=1d"
+        );
+      data = await this._delayedFetch(url, 500, 10000);
+    }
 
-    let url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?period1=${unixDate}&period2=${unixNextDate}&interval=1d`;
-    // let url = "https://api.allorigins.win/get?url=" + encodeURIComponent('https://query1.finance.yahoo.com/v8/finance/chart/'+ticker+'?period1='+unixDate+'&period2='+unixNextDate+'&interval=1d');
-    let response = await fetch(url);
+    // let response = await fetch(url);
 
-    let data = await response.json();
-    if (data.length === 0) throw new Error("No exchange rate data found");
+    // let data = await response.json();
+    // if (data.length === 0) throw new Error("No exchange rate data found");
     // console.log(data);
-    let exchangeRate = data["chart"]["result"][0]["indicators"]["quote"][0].close;
+
+    let exchangeRate =
+      data["chart"]["result"][0]["indicators"]["quote"][0].close;
     return exchangeRate;
   }
 
   static async search(query) {
-    let url = `https://query2.finance.yahoo.com/v1/finance/search?q=${query}`//&lang=pt-BR&region=BR&corsDomain=finance.yahoo.com&callback=`;
-    // let url = "https://api.allorigins.win/get?url=" + encodeURIComponent('https://query2.finance.yahoo.com/v1/finance/search?q=' + query);
-    let response = await fetch(url);
-    let status = response.status;
-    console.log("Status: " + status);
-    let data = await response.json();
-
-    if (data.lenght == 0) throw new Error("No data found on search query");
+    let data;
+    let url;
+    try {
+      if(this.corsProblem) { throw new Error("CORS problem"); }
+      console.log("Query: " + query);
+      url = `https://query2.finance.yahoo.com/v1/finance/search?q=${query}`; //&lang=pt-BR&region=BR&corsDomain=finance.yahoo.com&callback=`;
+      let response = await fetch(url);
+      data = await response.json();
+    } catch (error) {
+      this.corsProblem = true;
+      console.log("Query: " + query);
+      url =
+        "https://api.allorigins.win/get?url=" +
+        encodeURIComponent(
+          "https://query2.finance.yahoo.com/v1/finance/search?q=" + query
+        );
+      data = await this._delayedFetch(url, 500, 10000);
+    }
 
     return data;
-  
+  }
+
+  static async _delayedFetch(url, delay, fallbackDelay) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      let response = await fetch(url);
+      let data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("❌ Fetch failed:", error);
+      console.warn("Retrying in 10 seconds...");
+      await new Promise((resolve) => setTimeout(resolve, fallbackDelay));
+      return await this._delayedFetch(query);
+    }
   }
 
   static async getAssetType(isin) {
     let data = await this.search(isin);
+    data = JSON.parse(data.contents);
     return data.quotes[0].quoteType;
   }
 }
