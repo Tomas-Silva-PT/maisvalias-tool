65c146902e21840a5892ade6d1e2b81f187d2070
Tomas-Silva-PT <81096021+Tomas-Silva-PT@users.noreply.github.com>
2026-04-18 19:52:20 +0100

    fix(website): change order of columns for IRS panel 9.2
    So it correctly shows realization values first and acquisition values later

--- src/models/irs/panel.ts
+++ src/models/irs/panel.ts
@@ -1,14 +1,14 @@
 type AnexoJQuadro92A = {
   "País da fonte": string;
   "Código": string;
-  "Ano de Aquisição": number;
-  "Mês de Aquisição": number;
-  "Dia de Aquisição": number;
-  "Valor de Aquisição": number;
   "Ano de Realização": number;
   "Mês de Realização": number;
   "Dia de Realização": number;
   "Valor de Realização": number;
+  "Ano de Aquisição": number;
+  "Mês de Aquisição": number;
+  "Dia de Aquisição": number;
+  "Valor de Aquisição": number;
   "Despesas e Encargos": number;
   "Imposto retido no estrangeiro": number;
   "País da Contraparte": string;
